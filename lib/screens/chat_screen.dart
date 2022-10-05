635f8a9b99d714dfc024a618aa9d38714216bf58
nayemalam110 <nayemalam110@gmail.com>
2022-10-06 00:19:43 +0600

    step-2

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,24 +1,64 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
+  TextEditingController _textEditingController = TextEditingController();
+
+  addData(String text, ctx) {
+    Navigator.of(ctx).pop();
+    FirebaseFirestore.instance
+        .collection('chats/3okFKLl8GKUe9SMHiY7i/massages')
+        .add({'text': _textEditingController.text});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            return Center(child: Text("Hello"));
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('chats/3okFKLl8GKUe9SMHiY7i/massages')
+              .snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            var spdata = snapshot.data.docs;
+
+            return ListView.builder(
+              itemCount: spdata.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Center(child: Text(spdata[index]['text']));
+              },
+            );
           },
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            FirebaseFirestore.instance
-                .collection('chats/3okFKLl8GKUe9SMHiY7i/massages')
-                .snapshots()
-                .listen((event) {
-              print(event.docChanges[0].doc['text']);
-            });
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text('Add Massage'),
+                  content: TextField(
+                    controller: _textEditingController,
+                  ),
+                  actions: [
+                    TextButton(
+                        onPressed: () =>
+                            addData(_textEditingController.text, context),
+                        child: Text('Add'))
+                  ],
+                );
+              },
+            );
           },
           child: Icon(Icons.add),
         ));
