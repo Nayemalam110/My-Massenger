dde427f0357c6548e1e260fa32cbf95e8b2cc7a1
nayemalam110 <nayemalam110@gmail.com>
2022-10-13 08:01:41 +0600

    step-2.4

--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -7,14 +5,17 @@ class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('chats').snapshots(),
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(
             child: CircularProgressIndicator(),
           );
         }
-        final chatDocs = snapshot.data.documents;
+        if (!snapshot.hasData) {
+          return Center(child: Text("No Data"));
+        }
+        final chatDocs = snapshot.data.docs;
         return ListView.builder(
           itemCount: chatDocs.length,
           itemBuilder: (context, index) => Text(chatDocs[index]['text']),
---
+++ lib/widgets/new_message.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({Key? key}) : super(key: key);
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
