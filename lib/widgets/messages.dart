a99cb17ba42f7ff731be8931619b72818beec37b
nayemalam110 <nayemalam110@gmail.com>
2022-10-13 14:54:32 +0600

    step-2.4.2

--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -5,7 +5,10 @@ class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(
--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -15,6 +15,7 @@ class _NewMessageState extends State<NewMessage> {
     FocusScope.of(context).unfocus();
     FirebaseFirestore.instance.collection('chat').add({
       'text': _enteredMessage,
+      'createdAt': Timestamp.now(),
     });
 
     _controller.clear();
